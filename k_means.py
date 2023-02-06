ef56d9e9750a219e77bbe891193873f6f2e0d3c9
Mohammad Ali Khodayari <makhodayari99@gmail.com>
2023-02-06 23:59:59 +0330

    K-Means For Dataset 5

--- k_means.py
+++ k_means.py
@@ -1,16 +1,16 @@
 import utilities as utl
-import numpy as np
-import pandas as pd
+
 
 if __name__ == '__main__':
-
-    path = ['./datasets/blobs.csv', './datasets/circle.csv', './datasets/elliptical.csv', './datasets/moon.csv']
-    
-    for i in range(4):
+    path = ['./datasets/blobs.csv', './datasets/circle.csv', './datasets/elliptical.csv',
+            './datasets/moon.csv', './datasets/tsnv.csv']
+    for i in range(len(path)):
         data = utl.load_data(path[i])
-        print(data)
-        X = data.iloc[:, :-1]
-        X = utl.normalize(np.asanyarray(X))
+        if i == 4:
+            X = data.iloc[:, :]
+        else:
+            X = data.iloc[:, :-1]
+        X = utl.normalize(X).values
         WCSS = []
         for K in range(1, 15):
             clusters, means = utl.k_mans(X, K, 300)
